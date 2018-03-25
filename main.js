d1e5878c71400cb6c245a1d8dd1778be630750f4
Sara del Arco <sadelarar@gmail.com>
2018-03-26 00:43:04 +0200

    primer commit

---
+++ main.js
@@ -0,0 +1,30 @@
+$("#img1").mouseout (function(){
+    $("#img1").attr ("src","images/peniscola.jpg");
+});
+$("#img1").mouseover (function(){
+    $("#img1").attr ("src","images/peniscola.png");
+});
+
+
+$("#img2").mouseout (function(){
+    $("#img2").attr ("src","images/benicasim3.jpg");
+});
+$("#img2").mouseover (function(){
+    $("#img2").attr ("src","images/benicasim3.png");
+});
+
+
+$("#img3").mouseout (function(){
+    $("#img3").attr ("src","images/castellon.jpeg");
+});
+$("#img3").mouseover (function(){
+    $("#img3").attr ("src","images/castellon.png");
+});
+
+$("#img4").mouseout (function(){
+    $("#img4").attr ("src","images/canet2.jpg");
+});
+$("#img4").mouseover (function(){
+    $("#img4").attr ("src","images/canet2.png");
+});
+
